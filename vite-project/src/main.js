3e377cd9fc0b07ed8851f663fdef5d065b2091d7
kouta718 <qooron1927@gmail.com>
2025-09-09 23:48:46 +0900

    no message

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,1 +1,30 @@
 import './style.css'
+
+
+// 共通処理
+async function fetchPokemonData(slot) {
+    const input = document.querySelector(`#${slot}-species`);
+    const abilitySelect = document.querySelector(`#${slot}-ability`);
+
+    const name = input.value.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    // 特性セット
+    abilitySelect.innerHTML = "";
+    data.abilities.forEach(ability => {
+    const opt = document.createElement("option");
+    opt.value = ability.ability.name;
+    opt.textContent = ability.ability.name;
+    abilitySelect.appendChild(opt);
+    });
+}
+
+// まとめてイベント登録
+document.querySelectorAll(".searchBtn").forEach(btn => {
+    btn.addEventListener("click", (e) => {
+    const slot = e.target.closest(".pokemon-slot").dataset.slot;
+    fetchPokemonData(slot);
+    });
+});
